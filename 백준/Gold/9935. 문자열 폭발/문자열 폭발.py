4199850287334097c9ac407b48da54310aa846b4
zxcv718 <tkim0156@gmail.com>
2025-09-18 13:33:13 +0900

    [Gold IV] Title: 문자열 폭발, Time: 736 ms, Memory: 43596 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -5,9 +5,9 @@ stack = []
 for char in string:
     exp_len = len(exp_str)
     stack.append(char)
-    if len(stack) >= exp_len and ''.join(stack[-exp_len:]) == exp_str:
-        del stack[-exp_len:]
-
+    if ''.join(stack[-exp_len:]) == exp_str:
+        for _ in range(exp_len):
+            stack.pop()
 if stack:
     print(''.join(stack))
 else:
