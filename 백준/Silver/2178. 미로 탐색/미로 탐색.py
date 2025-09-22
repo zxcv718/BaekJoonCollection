dab42656c00dd4cde9de49c3b92a8568682f348b
zxcv718 <tkim0156@gmail.com>
2025-09-22 20:47:22 +0900

    [Silver I] Title: 미로 탐색, Time: 104 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,34 +1,35 @@
-#  https://www.acmicpc.net/problem/2178
 import sys
 from collections import deque
 
+input = sys.stdin.readline
 
-N, M = map(int, input().split())
-graph = []
-visit = [[False] * M for _ in range(N)]
-time_list = [[1] * M for _ in range(N)]
-for _ in range(N):
-    graph.append(list(map(int, list(sys.stdin.readline().strip()))))
+n, m = map(int, input().split())
+maze = [list(map(int, input().strip())) for _ in range(n)]
 
+dist = [[0] * m for _ in range(n)] # 0이면 미방문, 그리고 거기에 1씩 더해서 거리까지 한번에
+dist[0][0] = 1 # 시작점은 1,1로 고정, 따라서 시작과 동시에 방문하기 때문에 1
+
+queue = deque()
+queue.append((0,0))
+# 상, 하, 좌, 우 방향을 한 번에 처리하기 위한 델타 배열
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-queue = deque()
-queue.append((0, 0))
-visit[0][0] = True
 
 while queue:
-    node = queue.popleft()
-    y, x = node[0], node[1]
+    x, y = queue.popleft()
+    if x == n - 1 and y == m - 1:
+        break
 
     for i in range(4):
-        tx = x + dx[i]
-        ty = y + dy[i]
-        if (0 <= tx < M) and (0 <= ty < N):
-            if graph[ty][tx]:
-                if not visit[ty][tx]:
-                    visit[ty][tx] = 1
-                    time_list[ty][tx] = time_list[y][x] + 1
-                    queue.append((ty, tx))
+        nx = x + dx[i]
+        ny = y + dy[i]
 
+        if not (0 <= nx < n and 0 <= ny < m): # 범위 넘어서면 다음 조건식으로 이동
+            continue
+        if maze[nx][ny] == 0 or dist[nx][ny] != 0: # 갈수없는 칸이거나 방문했던 칸이면 다음 조건식으로 이동
+            continue
 
-print(time_list[-1][-1])
+        dist[nx][ny] = dist[x][y] + 1 # 거리 1씩 누적
+        queue.append((nx, ny))
+
+print(dist[-1][-1])
