124433955403a3a62326754aced690d5452fde54
zxcv718 <tkim0156@gmail.com>
2025-09-30 21:23:12 +0900

    [Gold IV] Title: 점프, Time: 1132 ms, Memory: 162820 KB -BaekjoonHub

---
+++ 백준/Gold/2253. 점프/점프.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+small = set()
+
+for _ in range(m):
+    s = int(input())
+    small.add(s)
+
+q = deque()
+dist = {(1, 0): 0}
+q.append((1, 0))
+candidate = [-1, 0, 1]
+
+def bfs():
+    while q:
+        s, prev = q.popleft()
+        
+        if s == n:
+            print(dist[(s, prev)])
+            return
+        
+        for i in candidate:
+            next = prev + i
+            nStone = s + next
+            
+            if nStone > n:
+                break
+            
+            if next > 0 and nStone not in small:
+                if (nStone, next) not in dist:
+                    dist[(nStone, next)] = dist[(s, prev)] + 1
+                    q.append((nStone, next))
+    
+    print(-1)
+
+bfs()
